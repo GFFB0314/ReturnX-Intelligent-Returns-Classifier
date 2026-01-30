e1016ef2b39c1245fb07b325374da1efbefb438b
GFFB0314 <gbetnkom.bechir@gmail.com>
2026-01-30 01:01:30 +0100

    Text cleaned up. Ready for TF-IDF

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import seaborn as sns
 from sklearn.feature_extraction.text import CountVectorizer
 from wordcloud import WordCloud
 from typing import Tuple
+from IPython.display import display
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
 
@@ -17,7 +18,7 @@ def visualize_ngrams(df: pd.DataFrame, category_name: str, ngram_range: Tuple[in
     Visualizes the top N-grams for a specific return category.
     Args:
         df: The DataFrame containing the reviews and categories
-        category_name: The class to analyze (e.g., 'Defect', 'Sizing')
+        category_name: The class to analyze (e.g., "Defect", "Sizing")
         n_gram_range: (2,2) for bigrams, (3,3) for trigrams
     """
 
@@ -64,12 +65,69 @@ def visualize_ngrams(df: pd.DataFrame, category_name: str, ngram_range: Tuple[in
         data=freq_distribution
     )
 
-    plt.title(f"Top {top_k} {'Bigrams' if ngram_range==(2,2) else 'Trigrams'} for: {category_name}")
+    plt.title(f"Top {top_k} {'Bigrams' if ngram_range==(2,2) else 'Trigrams'} for: {category_name} Before Cleaning")
     plt.xlabel("Frequency")
     plt.ylabel("Phrase")
     plt.show()
 
 
+
+def visualize_ngrams_clean(df: pd.DataFrame, category_name: str, ngram_range: Tuple[int, int] = (2, 2), top_k: int = 10):
+    """
+    Visualizes the top N-grams for a specific return category.
+    Args:
+        df: The DataFrame containing the reviews and categories
+        category_name: The class to analyze (e.g., "Defect", "Sizing")
+        n_gram_range: (2,2) for bigrams, (3,3) for trigrams
+    """
+
+    # 1. Filter the specific category
+    subset = df[df["return_category"] == category_name]
+
+    if subset.empty:
+        print(f"No data for category: {category_name}")
+        return 
+    
+    # 2. Initialize Vectorizer (Remove English Stop words)
+    # We use a custom token pattern to keep it clean
+    vectorizer = CountVectorizer(
+        ngram_range=ngram_range,
+        stop_words=None,
+        max_features=5000
+    ) # Bag of Words Model
+
+    # 3. Fit and Transform
+    try:
+        X = vectorizer.fit_transform(subset["clean_text"])
+    except ValueError:
+        # Handle cases where stop words might remove everything
+        print(f"Skipping {category_name}: Not enough valid text data.")
+        return
+
+    # 4. Sum up the counts of each n-gram
+    counts = np.asarray(X.sum(axis=0)).flatten()
+
+    vocab = vectorizer.get_feature_names_out()
+
+    freq_distribution = (
+        pd.DataFrame({"ngram": vocab, "count": counts})
+        .sort_values("count", ascending=False)
+        .head(top_k)
+    )
+
+    # 5. Ploting the Bar Chart
+    plt.figure(figsize=(10, 6))
+
+    sns.barplot(
+        x="count",
+        y="ngram",
+        data=freq_distribution
+    )
+
+    plt.title(f"Top {top_k} {'Bigrams' if ngram_range==(2,2) else 'Trigrams'} for: {category_name} After Cleaning")
+    plt.xlabel("Frequency")
+    plt.ylabel("Phrase")
+    plt.show()
 
 
 def plot_wordcloud(df: pd.DataFrame, category_name: str):
@@ -93,3 +151,29 @@ def plot_wordcloud(df: pd.DataFrame, category_name: str):
     plt.title(f"Word Cloud: {category_name}", fontweight="bold", fontsize=16)
     plt.show()
     
+
+def summarize_df(df, df_name="df"):
+    """
+    Display key information about a DataFrame:
+    - info()
+    - describe()
+    - duplicated rows
+    - count of missing values
+    """
+    print(f"===== DataFrame ({df_name.upper()}) Summary =====")
+    print("===== DataFrame Index =====")
+    display(df.index)
+    print("===== DataFrame Info =====")
+    df.info()
+    print("\n===== DataFrame Description =====")
+    display(
+        df.describe(include="all")
+    )  # include='all' to describe non-numeric columns too
+    print("\n===== Duplicate Rows =====")
+    duplicates = df[df.duplicated(keep=False)]
+    if not duplicates.empty:
+        display(duplicates)
+    else:
+        print("No duplicate rows found.")
+    print("\n===== Missing Values per Column =====")
+    print(df.isna().sum())
